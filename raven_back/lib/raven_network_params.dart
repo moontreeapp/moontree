2b19b832cdaf12e49bbfb3ab98752cea19b0db9f
Duane Johnson <duane.johnson@gmail.com>
2021-06-14 16:32:53 -0600

    Remove duplicate instantiated network params

--- raven_back/lib/raven_network_params.dart
+++ raven_back/lib/raven_network_params.dart
@@ -23,27 +23,3 @@ class RavenNetworkParams {
     return 'RavenNetworkParams{name: $name, testnet: $testnet, network: ${network.toString()}, derivationPath: $derivationPath';
   }
 }
-
-final ravencoinMainnet = RavenNetworkParams(
-    name: 'Ravencoin Mainnet',
-    testnet: false,
-    network: NetworkType(
-        messagePrefix: '\x16Raven Signed Message:\n',
-        bech32: 'rc',
-        bip32: Bip32Type(public: 0x0488b21e, private: 0x0488ade4),
-        pubKeyHash: 0x3c,
-        scriptHash: 0x7a,
-        wif: 0x80),
-    derivationBase: "m/44'/175'/0'");
-
-final ravencoinTestnet = RavenNetworkParams(
-    name: 'Ravencoin Testnet',
-    testnet: true,
-    network: NetworkType(
-        messagePrefix: '\x16Raven Signed Message:\n',
-        bech32: 'tr',
-        bip32: Bip32Type(public: 0x043587cf, private: 0x04358394),
-        pubKeyHash: 0x6f,
-        scriptHash: 0xc4,
-        wif: 0xef),
-    derivationBase: "m/44'/175'/1'");
