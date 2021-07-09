72d092d1bb4d1e1bf9f3eedbd6b9da94a4efc888
Duane Johnson <duane.johnson@gmail.com>
2021-07-09 11:44:14 -0600

    Add AES cipher (encrypt/decrypt)

---
+++ raven_back/lib/cipher.dart
@@ -0,0 +1,35 @@
+import 'dart:typed_data';
+
+import 'package:pointycastle/export.dart';
+
+Uint8List getBytes(String key) => Uint8List.fromList(key.codeUnits);
+
+// AES initialization vector; must be 16 bytes
+Uint8List defaultInitializationVector = getBytes('aeree5Zaeveexooj');
+
+class Cipher {
+  final Uint8List _key;
+  final Uint8List _iv;
+
+  Cipher(Uint8List key, {Uint8List? iv})
+      : _key = key,
+        _iv = iv ?? defaultInitializationVector;
+
+  PaddedBlockCipherImpl cipher(encrypt) {
+    var blockCipher = CBCBlockCipher(AESFastEngine());
+    var params = ParametersWithIV<KeyParameter>(KeyParameter(_key), _iv);
+    var paddingParams =
+        PaddedBlockCipherParameters<ParametersWithIV<KeyParameter>, Null>(
+            params, null);
+    return PaddedBlockCipherImpl(PKCS7Padding(), blockCipher)
+      ..init(encrypt, paddingParams);
+  }
+
+  Uint8List encrypt(Uint8List plainText) {
+    return cipher(true).process(plainText);
+  }
+
+  Uint8List decrypt(Uint8List cipherText) {
+    return cipher(false).process(cipherText);
+  }
+}
