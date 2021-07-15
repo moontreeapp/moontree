ddce51d47a364b762a03956b8acb75c4779c0461
Jordan Miller <jordan.kay@gmail.com>
2021-07-15 14:17:49 -0600

    creating template

--- raven_front/lib/main.dart
+++ raven_front/lib/main.dart
@@ -1,113 +1,164 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+//import 'package:bip39/bip39.dart' as bip39;
+//import 'package:raven_electrum_client/raven_electrum_client.dart';
+//import 'package:raven/boxes.dart';
+//import 'package:raven/account.dart';
+//import 'package:raven/accounts.dart';
+//import 'package:raven/env.dart' as env;
+//import 'package:raven/listen.dart' as listen;
+//import 'pages/loading.dart';
+//import 'pages/home.dart';
+//import 'pages/choose_account.dart';
 
-void main() {
-  runApp(MyApp());
+//Future setup() async {
+//  await Directory('database').delete(recursive: true);
+//  await Truth.instance.open();
+//  await Accounts.instance.load();
+//}
+//
+//void listenTo(RavenElectrumClient client) {
+//  listen.toAccounts();
+//  listen.toNodes(client);
+//  listen.toUnspents();
+//}
+
+Future<void> main() async {
+  // any setup stuff should happen on loading screen...
+  //await setup();
+  //var client = await RavenElectrumClient.connect('testnet.rvn.rocks');
+  //listenTo(client);
+  //var phrase = await env.getMnemonic();
+  //var account = Account.bySeed(ravencoinTestnet, bip39.mnemonicToSeed(phrase));
+  //await Truth.instance.saveAccount(account);
+  //await Truth.instance.unspents
+  //    .watch()
+  //    .skipWhile((element) =>
+  //        element.key !=
+  //        '0d78acdf5fe186432cbc073921f83bb146d72c4a81c6bde21c3003f48c15eb38')
+  //    .take(1)
+  //    .toList();
+
+  // should happen on home screen...
+  runApp(MaterialApp(home: RavenApp()));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class RavenApp extends StatefulWidget {
+  @override
+  _RavenAppState createState() => _RavenAppState();
+}
+
+class _RavenAppState extends State<RavenApp> {
+  //Accounts accounts = Accounts.instance;
+  final List<String> accounts = [
+    'a',
+    'b',
+    'c',
+    'a',
+    'b',
+    'c',
+    'a',
+    'b',
+    'c',
+    'a',
+    'b',
+    'c',
+    'a',
+    'b',
+    'c',
+  ];
+  late String chosen = 'a';
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('RavenApp'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text('What is needed'),
+            Divider(),
             Text(
-              'You have pushed the button this many times:',
+              'Name:',
+              style: const TextStyle(fontWeight: FontWeight.bold),
             ),
+            Text(chosen),
+            Text('edit button'),
+            Text(''),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'Balance:',
+              style: const TextStyle(fontWeight: FontWeight.bold),
             ),
+            Text('balance'),
+            Text(''),
+            Text(
+              'Sync:',
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text('latest activity'),
+            Text('refresh'),
+            Text(''),
+            Text(
+              'Security:',
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text('See account backup phrase'),
+            Text(''),
+            Text(
+              'Transactions:',
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text('List of Transactions'),
+            Text('Send'),
+            Text('Receive'),
+            Text(''),
+            Text(
+              'Assets:',
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text('Create Asset'),
+            Text('See list of assets'),
+            Text('Trade Assets'),
+            Text(''),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            ...[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Column(
+                  children: <Widget>[Text('Accounts'), Text('overall Balance')],
+                ),
+              )
+            ],
+            ...accounts
+                .map(
+                  (e) => ListTile(
+                    title: Text(e),
+                    onTap: () {
+                      setState(() {
+                        chosen = e;
+                      });
+                    },
+                  ),
+                )
+                .toList()
+          ],
+        ),
+      ),
     );
   }
 }
