a94dc4666e87cd0b9f7d91ae20684caac54f2fc5
Jordan Miller <jordan.kay@gmail.com>
2021-07-15 09:20:05 -0600

    using ravencoins networks

--- raven_back/lib/raven_networks.dart
+++ raven_back/lib/raven_networks.dart
@@ -1,26 +1,15 @@
+import 'package:raven/box_adapters.dart';
 import 'package:ravencoin/ravencoin.dart';
 import 'network_params.dart';
 
 final ravencoinMainnet = NetworkParams(
     name: 'Ravencoin Mainnet',
     testnet: false,
-    network: NetworkType(
-        messagePrefix: '\x16Raven Signed Message:\n',
-        bech32: 'rc',
-        bip32: Bip32Type(public: 0x0488b21e, private: 0x0488ade4),
-        pubKeyHash: 0x3c,
-        scriptHash: 0x7a,
-        wif: 0x80),
+    network: ravencoin,
     derivationBase: "m/44'/175'/0'");
 
-late final ravencoinTestnet = NetworkParams(
+final ravencoinTestnet = NetworkParams(
     name: 'Ravencoin Testnet',
     testnet: true,
-    network: NetworkType(
-        messagePrefix: '\x16Raven Signed Message:\n',
-        bech32: 'tr',
-        bip32: Bip32Type(public: 0x043587cf, private: 0x04358394),
-        pubKeyHash: 0x6f,
-        scriptHash: 0xc4,
-        wif: 0xef),
+    network: testnet,
     derivationBase: "m/44'/175'/1'");
