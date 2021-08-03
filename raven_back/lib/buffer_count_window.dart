0395ef1fd033f1eaf3647a75b2da7a519e816b5a
Duane Johnson <duane.johnson@gmail.com>
2021-08-03 09:04:34 -0600

    Document rxdart extension bufferCountTimeout

--- raven_back/lib/buffer_count_window.dart
+++ raven_back/lib/buffer_count_window.dart
@@ -3,12 +3,12 @@ import 'package:rxdart/src/transformers/backpressure/backpressure.dart';
 class BufferCountWindowStreamTransformer<T>
     extends BackpressureStreamTransformer<T, List<T>> {
   BufferCountWindowStreamTransformer(int count, Stream Function(T event) window,
-      [int startBufferEvery = 0])
+      [int startBufferEvery = 0, ignoreEmptyWindows = true])
       : super(WindowStrategy.firstEventOnly, window,
             onWindowEnd: (List<T> queue) => queue,
             startBufferEvery: startBufferEvery,
             closeWindowWhen: (Iterable<T> queue) => queue.length == count,
-            ignoreEmptyWindows: true) {
+            ignoreEmptyWindows: ignoreEmptyWindows) {
     if (count < 1) throw ArgumentError.value(count, 'count');
     if (startBufferEvery < 0) {
       throw ArgumentError.value(startBufferEvery, 'startBufferEvery');
@@ -17,10 +17,20 @@ class BufferCountWindowStreamTransformer<T>
 }
 
 extension BufferCountWindowExtensions<T> on Stream<T> {
-  Stream<List<T>> bufferCountWindow(int count, Stream window) =>
-      transform(BufferCountWindowStreamTransformer(count, (_) => window));
+  /// Buffers the stream and emits the buffer when EITHER:
+  /// a) the number of elements reaches `count`, OR
+  /// b) the `window` Stream emits
+  Stream<List<T>> bufferCountWindow(int count, Stream window,
+          {ignoreEmptyWindows = true}) =>
+      transform(BufferCountWindowStreamTransformer(
+          count, (_) => window, 0, ignoreEmptyWindows));
 
-  Stream<List<T>> bufferCountTimeout(int count, Duration timeout) {
-    return bufferCountWindow(count, Stream.periodic(timeout));
+  /// Buffers the stream and emits the buffer when EITHER:
+  /// a) the number of elements reaches `count`, OR
+  /// b) a periodic `timeout` occurs
+  Stream<List<T>> bufferCountTimeout(int count, Duration timeout,
+      {ignoreEmptyWindows = true}) {
+    transform(BufferCountWindowStreamTransformer(
+        count, (_) => Stream.periodic(timeout), 0, ignoreEmptyWindows));
   }
 }
