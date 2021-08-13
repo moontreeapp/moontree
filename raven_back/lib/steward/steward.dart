9897375fb9983f48d9953d84cac43bceb8ac0583
Jordan Miller <Jordan.kay@gmail.com>
2021-08-13 16:39:16 -0600

    made note about how to remove the need for stewards if we want

--- raven_back/lib/steward/steward.dart
+++ raven_back/lib/steward/steward.dart
@@ -1,5 +1,10 @@
 /// the point of the steward is to be a singleton that holds all long running objects
 /// I know this sounds crazy but. lets just give it a try. it's a singleton.
+///
+/// alternative to this is: we can create waiters in the same way we create
+/// the services and reservoirs at the beginning, and then pass the reservoirs
+/// into the waiters that need them in the beginning, and pass the aiters into
+/// the services that need them too.
 
 class Steward {
   //static final Steward _singleton = Steward._();
