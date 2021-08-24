00abba5e0a070b7af82f2ab6727b1680e64b972f
Duane Johnson <duane.johnson@gmail.com>
2021-08-24 16:46:22 -0230

    Separate WalletBase extension functions (scripthash, wif, keypair)

--- raven_back/lib/records/wallets/wallet.dart
+++ raven_back/lib/records/wallets/wallet.dart
@@ -1,28 +1,8 @@
-import 'dart:typed_data';
-
 import 'package:equatable/equatable.dart';
-import 'package:crypto/crypto.dart';
-import 'package:convert/convert.dart' show hex;
 import 'package:hive/hive.dart';
-import 'package:ravencoin/ravencoin.dart' show WalletBase, ECPair;
-import 'package:ravencoin/ravencoin.dart' as rc;
 import 'package:raven/utils/cipher.dart';
 
-extension Scripthash on WalletBase {
-  Uint8List get outputScript {
-    return rc.Address.addressToOutputScript(address!, network)!;
-  }
-
-  String get scripthash {
-    var digest = sha256.convert(outputScript);
-    var hash = digest.bytes.reversed.toList();
-    return hex.encode(hash);
-  }
-
-  ECPair get keyPair {
-    return ECPair.fromWIF(wif!, networks: rc.networks);
-  }
-}
+export 'extended_wallet_base.dart';
 
 abstract class Wallet with HiveObjectMixin, EquatableMixin {
   @HiveField(0)
