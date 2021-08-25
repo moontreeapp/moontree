eb065619f8fb75397e4073a360f48facb37e2529
Jordan Miller <Jordan.kay@gmail.com>
2021-08-25 12:27:31 -0600

    statuses are nullable

--- raven_electrum/lib/subscriptions/subscribe_scripthash.dart
+++ raven_electrum/lib/subscriptions/subscribe_scripthash.dart
@@ -1,11 +1,8 @@
-import 'package:equatable/equatable.dart';
-import 'package:json_rpc_2/json_rpc_2.dart';
-
 import '../client/subscribing_client.dart';
 import '../raven_electrum_client.dart';
 
 extension SubscribeScripthashMethod on RavenElectrumClient {
-  Stream<String> subscribeScripthash(String scripthash) {
+  Stream<String?> subscribeScripthash(String scripthash) {
     var methodPrefix = 'blockchain.scripthash';
 
     // If this is the first time, register
