71beec7310bc2134d9f3ebedd38436e00efc84de
Duane Johnson <duane.johnson@gmail.com>
2021-09-15 10:54:46 -0600

    Add note to argone2 key deriv about how to tweak params

--- raven_back/lib/utils/cipher.dart
+++ raven_back/lib/utils/cipher.dart
@@ -19,6 +19,7 @@ class NoCipher implements Cipher {
 
   @override
   Uint8List encrypt(Uint8List plainText) => plainText;
+
   @override
   Uint8List decrypt(Uint8List cipherText) => cipherText;
 }
@@ -28,7 +29,7 @@ class AESCipher implements Cipher {
   late Uint8List _iv; // 16 bytes
 
   AESCipher(Uint8List password, {Uint8List? salt}) {
-    var generator = keyGenerator(48, salt);
+    var generator = makeKeyGenerator(48, salt);
     var result = generator.process(password);
     assert(result.length == 48);
     _key = result.sublist(0, 32);
@@ -38,8 +39,10 @@ class AESCipher implements Cipher {
   /// While passwords can be any length, AES block ciphers require a specific
   /// key length. Using the Argon2 algorithm, we produce a generator that can
   /// convert a variable-length password into a byte array of specific length.
-  Argon2BytesGenerator keyGenerator(int length, [Uint8List? salt]) =>
+  Argon2BytesGenerator makeKeyGenerator(int length, [Uint8List? salt]) =>
       Argon2BytesGenerator()
+        // Note: we may need to tweak these params to work best on mobile devices
+        //       https://www.twelve21.io/how-to-choose-the-right-parameters-for-argon2/
         ..init(Argon2Parameters(
           Argon2Parameters.ARGON2_id,
           salt ?? DEFAULT_SALT,
