7f5a7ed1c8babc6437781da16d3b12aaeac173be
Duane Johnson <duane.johnson@gmail.com>
2021-09-22 10:27:47 -0600

    Rename settings -> setting service

--- raven_back/lib/services/services.dart
+++ raven_back/lib/services/services.dart
@@ -3,7 +3,7 @@ import 'address.dart';
 import 'balance.dart';
 import 'history.dart';
 import 'rate.dart';
-import 'settings.dart';
+import 'setting.dart';
 import 'wallet.dart';
 
 class services {
