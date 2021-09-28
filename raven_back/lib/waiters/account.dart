a1b8facb17c921f578094f4e4d357dfcefa3b694
Duane Johnson <duane.johnson@gmail.com>
2021-09-28 12:00:48 -0600

    Add initial account unmatched pairs matcher

--- raven_back/lib/waiters/account.dart
+++ raven_back/lib/waiters/account.dart
@@ -2,6 +2,20 @@ import 'package:reservoir/reservoir.dart' show Change;
 
 import 'package:raven/waiters/waiter.dart';
 import 'package:raven/raven.dart';
+import 'package:rxdart/subjects.dart';
+
+// Set<bool> loggedIn = Set();
+
+bool loggedIn = false;
+
+Set<Account> unmatchedAccounts = {};
+
+// class AccountAndCipher {
+//   final Account? account;
+//   final bool? login; // true
+
+//   AccountAndLogin(this.account, this.login);
+// }
 
 class AccountWaiter extends Waiter {
   void init() {
@@ -18,9 +32,20 @@ class AccountWaiter extends Waiter {
   // add stuff to backlog
   // once login do backlog and erase
 
+  BehaviorSubject loggedInAccountsSubject = BehaviorSubject();
+
   void setupListeners() {
     /// this listener implies we have to load everthing backwards if importing:
     /// first balances, histories, addresses, wallets and then accounts
+    subjects.login.stream.listen((login) async {
+      if (login) {
+        loggedIn = true;
+        unmatchedAccounts.forEach((account) {
+          loggedInAccountsSubject.sink.add(account);
+        });
+      }
+    });
+
     if (!listeners.keys.contains('accounts.changes')) {
       listeners['accounts.changes'] =
           accounts.changes.listen((List<Change> changes) {
@@ -28,11 +53,19 @@ class AccountWaiter extends Waiter {
           change.when(
               added: (added) {
                 var account = added.data;
-                if (wallets.byAccount.getAll(account.accountId).isEmpty) {
-                  services.wallets.leaders.makeSaveLeaderWallet(
-                      account.accountId, cipherRegistry.currentCipher,
-                      cipherUpdate: cipherRegistry.currentCipherUpdate);
+
+                if (loggedIn) {
+                  loggedInAccountsSubject.sink.add(account);
+                } else {
+                  unmatchedAccounts.add(account);
                 }
+
+                // var account = added.data;
+                // if (wallets.byAccount.getAll(account.accountId).isEmpty) {
+                //   services.wallets.leaders.makeSaveLeaderWallet(
+                //       account.accountId, cipherRegistry.currentCipher,
+                //       cipherUpdate: cipherRegistry.currentCipherUpdate);
+                // }
               },
               updated: (updated) {},
               removed: (removed) {});
