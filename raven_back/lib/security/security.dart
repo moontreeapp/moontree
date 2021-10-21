ddfae2b5d40a10176ffd63dbcd303a0a2d44b992
Duane Johnson <duane.johnson@gmail.com>
2021-10-21 15:53:21 -0600

    Remove unused CipherRegistry and Wallet waiter

--- raven_back/lib/security/security.dart
+++ raven_back/lib/security/security.dart
@@ -1,7 +1,6 @@
 export 'cipher_base.dart';
 export 'cipher_aes.dart';
 export 'cipher_none.dart';
-export 'cipher_registry.dart';
 
 export 'encrypted_entropy.dart';
 export 'encrypted_wallet_secret.dart';
