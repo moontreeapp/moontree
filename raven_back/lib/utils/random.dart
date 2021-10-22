a046278ad813f03d5e78688c610b1c6d89e85271
Jordan Miller <Jordan.kay@gmail.com>
2021-10-22 17:07:11 -0600

    adding in memo WIp

--- raven_back/lib/utils/random.dart
+++ raven_back/lib/utils/random.dart
@@ -9,3 +9,12 @@ Uint8List randomBytes(int n) {
   }
   return random;
 }
+
+/// Generates a positive (psuedo-)random integer uniformly distributed on the
+/// range from [min], inclusive, to [max], exclusive.
+int randomInRange(int min, int max, [Random? generator]) =>
+    min + (generator ?? Random()).nextInt(max - min);
+
+/// return a (psuedo-)random item from the iterable
+int ChooseAtRandom(Iterable items, [Random? generator]) =>
+    items.toList()[(generator ?? Random()).nextInt(items.length)];
