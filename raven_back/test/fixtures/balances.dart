5e21df8e2b16396750d1a1c79edf98ac7f002c28
Duane Johnson <duane.johnson@gmail.com>
2021-10-26 16:51:42 -0600

    Use new securities constants in balance fixtures

--- raven_back/test/fixtures/balances.dart
+++ raven_back/test/fixtures/balances.dart
@@ -1,3 +1,4 @@
+import 'package:raven/globals.dart';
 import 'package:reservoir/map_source.dart';
 
 import 'package:raven/records/records.dart';
@@ -5,9 +6,12 @@ import 'package:raven/records/records.dart';
 MapSource<Balance> balances() => MapSource({
       '0': Balance(
           walletId: '0',
-          security: RVN,
+          security: securities.RVN,
           confirmed: 15000000,
           unconfirmed: 10000000),
-      '1':
-          Balance(walletId: '0', security: USD, confirmed: 100, unconfirmed: 0),
+      '1': Balance(
+          walletId: '0',
+          security: securities.USD,
+          confirmed: 100,
+          unconfirmed: 0),
     });
--- raven_back/test/fixtures/fixtures.dart
+++ raven_back/test/fixtures/fixtures.dart
@@ -1,13 +1,13 @@
 export 'accounts.dart';
 export 'addresses.dart';
 export 'balances.dart';
-export 'histories.dart';
+// export 'histories.dart';
 export 'wallets.dart';
 
 import 'accounts.dart';
 import 'addresses.dart';
 import 'balances.dart';
-import 'histories.dart';
+// import 'histories.dart';
 import 'wallets.dart';
 
 import 'package:raven/globals.dart' as globals;
@@ -16,6 +16,6 @@ void useFixtureSources() {
   globals.accounts.setSource(accounts());
   globals.wallets.setSource(wallets());
   globals.addresses.setSource(addresses());
-  globals.histories.setSource(histories());
+  // globals.histories.setSource(histories());
   globals.balances.setSource(balances());
 }
