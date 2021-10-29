a83d007805d3ad2f0d783edb38893a73470f26bd
Duane Johnson <duane.johnson@gmail.com>
2021-10-29 15:31:36 -0600

    Move makeFirstWallet for account to services/account

--- raven_back/lib/waiters/account.dart
+++ raven_back/lib/waiters/account.dart
@@ -1,6 +1,4 @@
 import 'package:raven/records/cipher.dart';
-import 'package:raven/utils/streaming_joins.dart';
-import 'package:rxdart/rxdart.dart';
 import 'package:reservoir/reservoir.dart' show Change;
 
 import 'package:raven/waiters/waiter.dart';
@@ -10,63 +8,29 @@ class AccountWaiter extends Waiter {
   Set<Account> backlog = {};
 
   void init() {
-    // var addCipherStream = ciphers.changes.whereType<Added<Cipher>>();
-    //
-    // var stream = streamingLeftJoin(
-    //   ciphers.batchedChanges.flatMap<Cipher>((value) => null),
-    //   accounts.batchedChanges,
-    //   (List<Change<Cipher>> batchedChanges) {
-    //     return services.cipher.currentCipher == null ? 'null' : 'not-null';
-    //   },
-    //   (List<Change<Account>> batchedChanges) {
-    //     return 'not-null';
-    //   },
-    // );
-
-    // listen('ciphers/accounts', stream,
-    //     (Join<List<Change<Cipher>>, List<Change<Account>>> row) {});
-
-    listen('ciphers.batchedChanges', ciphers.batchedChanges,
-        (List<Change<Cipher>> batchedChanges) async {
-      batchedChanges.forEach((change) => change.when(
-          loaded: (loaded) {},
-          added: (added) {
-            if (added.data.cipher == services.cipher.currentCipher) {
-              backlog.forEach((account) {
-                makeFirstWallet(account);
-              });
-            }
-          },
-          updated: (updated) {},
-          removed: (removed) {}));
+    listen('ciphers.changes', ciphers.changes, (Change<Cipher> change) async {
+      if (change is Added) {
+        if (change.data.cipher == services.cipher.currentCipher) {
+          backlog.forEach((account) {
+            services.account
+                .makeFirstWallet(account, services.cipher.currentCipher2!);
+          });
+        }
+      }
     });
 
-    /// this listener implies we have to load everthing backwards if importing:
+    /// this listener implies we have to load everything backwards if importing:
     /// first balances, histories, addresses, wallets and then accounts
-    listen<List<Change<Account>>>(
-        'accounts.batchedChanges', accounts.batchedChanges, (batchedChanges) {
-      batchedChanges.forEach((change) {
-        change.when(
-            loaded: (loaded) {},
-            added: (added) {
-              var account = added.data;
-              if (services.cipher.currentCipher == null) {
-                backlog.add(account);
-              } else {
-                makeFirstWallet(account);
-              }
-            },
-            updated: (updated) {},
-            removed: (removed) {});
-      });
+    listen<Change<Account>>('accounts.changes', accounts.changes, (change) {
+      if (change is Added) {
+        var account = change.data;
+        if (services.cipher.currentCipher == null) {
+          backlog.add(account);
+        } else {
+          services.account
+              .makeFirstWallet(account, services.cipher.currentCipher2!);
+        }
+      }
     });
-  }
-
-  void makeFirstWallet(Account account) {
-    if (wallets.byAccount.getAll(account.accountId).isEmpty) {
-      services.wallet.leader.makeSaveLeaderWallet(
-          account.accountId, services.cipher.currentCipher!,
-          cipherUpdate: services.cipher.currentCipherUpdate);
-    }
   }
 }
