eab9d56937b9d7109ec9561f2fafacc18e30f8a0
Duane Johnson <duane.johnson@gmail.com>
2021-11-09 13:36:10 -0700

    Refactor About page as StatelessWidget

--- raven_front/lib/pages/settings/about.dart
+++ raven_front/lib/pages/settings/about.dart
@@ -1,27 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:raven_mobile/components/components.dart';
-import 'package:raven_mobile/utils/utils.dart';
 
-class About extends StatefulWidget {
-  final dynamic data;
-  const About({this.data}) : super();
-
-  @override
-  _AboutState createState() => _AboutState();
-}
-
-class _AboutState extends State<About> {
-  dynamic data = {};
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
+class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    data = populateData(context, data);
     return Scaffold(
       appBar: components.headers.back(context, 'About'),
       body: body(),
