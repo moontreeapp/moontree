287ebdb0f59bbd4c36569e203b86e7706bcd54f8
Jordan Miller <Jordan.kay@gmail.com>
2021-11-22 15:40:54 -0700

    added elements to secuiryt

--- raven_back/lib/records/security.dart
+++ raven_back/lib/records/security.dart
@@ -121,4 +121,6 @@ class Security with EquatableMixin {
           r'Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,}'));
 
   bool get hasMetadata => metadata != null && metadata != '';
+  bool get isMaster => symbol.endsWith('!');
+  String get nonMasterSymbol => symbol.replaceAll('!', '');
 }
