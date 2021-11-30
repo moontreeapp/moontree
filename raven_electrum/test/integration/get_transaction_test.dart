5d2db34dbfff97670ff63ff81d5affef07b42ab7
Duane Johnson <duane.johnson@gmail.com>
2021-11-30 15:25:46 -0700

    Fix test syntax error

--- raven_electrum/test/integration/get_transaction_test.dart
+++ raven_electrum/test/integration/get_transaction_test.dart
@@ -1,9 +1,13 @@
+import 'package:test/test.dart';
+import 'package:raven_electrum/raven_electrum.dart';
 
+void main() {
   test('getTransaction and parse', () async {
     var client = RavenElectrumClient(await connect('testnet.rvn.rocks'));
     await client.serverVersion(protocolVersion: '1.9');
     var tx = await client.getTransaction(
         'e86f693b46f1ca33480d904acd526079ba7585896cff6d0ae5dcef322d9dc52a');
-    expect(tx.vout[0].memo),
+    expect(tx.vout[0].memo,
         'aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf9');
   });
+}
