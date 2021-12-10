bb7ed24a2cf4a825fd0210c00f42207c88c06924
Jordan Miller <Jordan.kay@gmail.com>
2021-12-10 12:13:19 -0700

    #73 #75 receive screen should show name

--- raven_front/lib/pages/transaction/receive.dart
+++ raven_front/lib/pages/transaction/receive.dart
@@ -27,6 +27,7 @@ class _ReceiveState extends State<Receive> {
   FocusNode requestMessageFocus = FocusNode();
   bool rawAddress = true;
   String uri = '';
+  String username = '';
 
   void _toggleRaw(_) {
     setState(() {
@@ -45,7 +46,8 @@ class _ReceiveState extends State<Receive> {
         ? ''
         //: 'message=${Uri.encodeComponent(requestMessage.text)}';
         : 'message=asset:${Uri.encodeComponent(requestMessage.text)}';
-    var tail = [amount, label, message].join('&').replaceAll('&&', '&');
+    var to = username == '' ? '' : 'to=${Uri.encodeComponent(username)}';
+    var tail = [amount, label, message, to].join('&').replaceAll('&&', '&');
     tail =
         '?' + (tail.endsWith('&') ? tail.substring(0, tail.length - 1) : tail);
     tail = tail.length == 1 ? '' : tail;
@@ -69,6 +71,7 @@ class _ReceiveState extends State<Receive> {
 
   @override
   Widget build(BuildContext context) {
+    username = settings.primaryIndex.getOne(SettingName.User_Name)?.value ?? '';
     data = populateData(context, data);
     requestMessage.text = requestMessage.text == ''
         ? data['symbol'] == ''
@@ -91,142 +94,155 @@ class _ReceiveState extends State<Receive> {
         ));
   }
 
-  ListView body() => ListView(
-          shrinkWrap: true,
-          padding: EdgeInsets.all(10.0),
-          children: <Widget>[
-            /// if this is a RVNt account we could show that here...
-            //SizedBox(height: 15.0),
-            //Text(
-            //    // rvn is default but if balance is 0 then take the largest asset balance and also display name here.
-            //    'RVN',
-            //    textAlign: TextAlign.center,
-            //    style: Theme.of(context).textTheme.bodyText1),
-            SizedBox(height: 20.0),
-            Center(
-                child: QrImage(
-                    backgroundColor: Colors.white,
-                    data: rawAddress ? address : uri,
-                    version: QrVersions.auto,
-                    size: 300.0)),
-            SizedBox(height: 10.0),
-            Center(
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                  /// does not belong on UI but I still want an indication that what is on QR code is not raw address...
-                  Visibility(
-                      visible: !rawAddress,
-                      child: Text(
-                        'raven:',
-                        style: Theme.of(context).textTheme.caption,
-                      )),
-                  SelectableText(
-                    address,
-                    cursorColor: Colors.grey[850],
-                    showCursor: true,
-                    toolbarOptions: ToolbarOptions(
-                        copy: true, selectAll: true, cut: false, paste: false),
-                  ),
-                  Visibility(
-                      visible: !rawAddress && requestAmount.text != '',
-                      child: Text(
-                        'amount: ${requestAmount.text}',
-                        style: Theme.of(context).textTheme.caption,
-                      )),
-                  Visibility(
-                      visible: !rawAddress && requestLabel.text != '',
-                      child: Text(
-                        'label: ${requestLabel.text}',
-                        style: Theme.of(context).textTheme.caption,
-                      )),
-                  Visibility(
-                      visible: !rawAddress && requestMessage.text != '',
-                      child: Text(
-                        'message: ${requestMessage.text}',
-                        style: Theme.of(context).textTheme.caption,
-                      )),
-                ])),
-            SizedBox(height: 20.0),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Row(children: <Widget>[
-                    Checkbox(
-                      value: rawAddress,
-                      onChanged: (_) {
-                        _toggleRaw(_);
-                        _makeURI();
-                        FocusScope.of(context)
-                            .requestFocus(requestMessageFocus);
-                      },
-                    ),
-                    Text('address only'),
-                  ]),
-                  shareAddressButton(),
-                ]),
-            Visibility(
-                visible: !rawAddress,
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      SizedBox(height: 15),
-                      Text(
-                        'Requested Asset:',
-                        style: TextStyle(color: Theme.of(context).hintColor),
-                      ),
-                      DropdownButton<String>(
-                          focusNode: requestMessageFocus,
-                          isExpanded: true,
-                          value: requestMessage.text,
-                          items: Current.holdingNames
-                              .map((String value) => DropdownMenuItem<String>(
-                                  value: value, child: Text(value)))
-                              .toList(),
-                          onChanged: (String? newValue) {
-                            requestMessage.text = newValue!;
-                            _makeURI();
-                            FocusScope.of(context)
-                                .requestFocus(requestAmountFocus);
-                          }),
-                      SizedBox(height: 15.0),
-                    ])),
-            Visibility(
-                visible: !rawAddress,
-                child: TextField(
-                    focusNode: requestAmountFocus,
-                    autocorrect: false,
-                    controller: requestAmount,
-                    keyboardType: TextInputType.number,
-                    textInputAction: TextInputAction.done,
-                    decoration: InputDecoration(
-                        border: UnderlineInputBorder(),
-                        labelText: 'Amount (Optional)',
-                        hintText: 'Quantity'),
-                    onEditingComplete: () {
-                      requestAmount.text = verifyDecAmount(requestAmount.text);
-                      // send focus to next element
-                      _makeURI();
-                      FocusScope.of(context).requestFocus(requestLabelFocus);
-                    })),
-            Visibility(
-                visible: !rawAddress,
-                child: TextField(
-                  focusNode: requestLabelFocus,
-                  autocorrect: false,
-                  controller: requestLabel,
-                  decoration: InputDecoration(
-                      border: UnderlineInputBorder(),
-                      labelText: 'Label (Optional)',
-                      hintText: 'Groceries'),
-                  onEditingComplete: () {
+  ListView body() =>
+      ListView(shrinkWrap: true, padding: EdgeInsets.all(10.0), children: <
+          Widget>[
+        /// if this is a RVNt account we could show that here...
+        //SizedBox(height: 15.0),
+        //Text(
+        //    // rvn is default but if balance is 0 then take the largest asset balance and also display name here.
+        //    'RVN',
+        //    textAlign: TextAlign.center,
+        //    style: Theme.of(context).textTheme.bodyText1),
+        SizedBox(height: 20.0),
+        Center(
+            child: QrImage(
+                backgroundColor: Colors.white,
+                data: rawAddress ? address : uri,
+                version: QrVersions.auto,
+                size: 300.0)),
+        SizedBox(height: 10.0),
+        Center(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+              /// does not belong on UI but I still want an indication that what is on QR code is not raw address...
+              Visibility(
+                  visible: !rawAddress,
+                  child: Text(
+                    'raven:',
+                    style: Theme.of(context).textTheme.caption,
+                  )),
+              SelectableText(
+                address,
+                cursorColor: Colors.grey[850],
+                showCursor: true,
+                toolbarOptions: ToolbarOptions(
+                    copy: true, selectAll: true, cut: false, paste: false),
+              ),
+              Visibility(
+                  visible: !rawAddress && username != '',
+                  child: Text(
+                    'to: $username',
+                    style: Theme.of(context).textTheme.caption,
+                  )),
+              Visibility(
+                  visible: !rawAddress && requestAmount.text != '',
+                  child: Text(
+                    'amount: ${requestAmount.text}',
+                    style: Theme.of(context).textTheme.caption,
+                  )),
+              Visibility(
+                  visible: !rawAddress && requestLabel.text != '',
+                  child: Text(
+                    'label: ${requestLabel.text}',
+                    style: Theme.of(context).textTheme.caption,
+                  )),
+              Visibility(
+                  visible: !rawAddress && requestMessage.text != '',
+                  child: Text(
+                    'message: ${requestMessage.text}',
+                    style: Theme.of(context).textTheme.caption,
+                  )),
+            ])),
+        SizedBox(height: 20.0),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Row(children: <Widget>[
+                Checkbox(
+                  value: rawAddress,
+                  onChanged: (_) {
+                    _toggleRaw(_);
                     _makeURI();
-                    FocusScope.of(context).unfocus();
+                    FocusScope.of(context).requestFocus(requestMessageFocus);
                   },
-                )),
-          ]);
+                ),
+                Text('address only'),
+              ]),
+              shareAddressButton(),
+            ]),
+        Visibility(
+            visible: !rawAddress,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: 15),
+                  Text(
+                    'Requested Asset:',
+                    style: TextStyle(color: Theme.of(context).hintColor),
+                  ),
+                  Autocomplete<Security>(
+                    displayStringForOption: _displayStringForOption,
                     initialValue: TextEditingValue(text: requestMessage.text),
+                    optionsBuilder: (TextEditingValue textEditingValue) {
+                      requestMessage.text = textEditingValue.text;
+                      _makeURI();
+                      if (textEditingValue.text == '') {
+                        return const Iterable<Security>.empty();
+                      }
+                      return securities.data
+                          .where((Security option) => option.symbol
+                              .contains(textEditingValue.text.toUpperCase()))
+                          .toList()
+                          .reversed;
+                    },
+                    optionsMaxHeight: 100,
+                    onSelected: (Security selection) async {
+                      requestMessage.text = selection.symbol;
+                      _makeURI();
+                      FocusScope.of(context).requestFocus(requestAmountFocus);
+                    },
+                  ),
+                  SizedBox(height: 15.0),
+                ])),
+        Visibility(
+            visible: !rawAddress,
+            child: TextField(
+                focusNode: requestAmountFocus,
+                autocorrect: false,
+                controller: requestAmount,
+                keyboardType: TextInputType.number,
+                textInputAction: TextInputAction.done,
+                decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Amount (Optional)',
+                    hintText: 'Quantity'),
+                onEditingComplete: () {
+                  requestAmount.text = verifyDecAmount(requestAmount.text);
+                  // send focus to next element
+                  _makeURI();
+                  FocusScope.of(context).requestFocus(requestLabelFocus);
+                })),
+        Visibility(
+            visible: !rawAddress,
+            child: TextField(
+              focusNode: requestLabelFocus,
+              autocorrect: false,
+              controller: requestLabel,
+              decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Label (Optional)',
+                  hintText: 'Groceries'),
+              onEditingComplete: () {
+                _makeURI();
+                FocusScope.of(context).unfocus();
+              },
+            )),
+      ]);
+
+  static String _displayStringForOption(Security option) => option.symbol;
 
   ElevatedButton shareAddressButton() => ElevatedButton.icon(
         icon: Icon(Icons.share),
