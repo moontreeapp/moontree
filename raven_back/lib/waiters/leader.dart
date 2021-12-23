37d7eb3d63159d24df8146dff2efd2892bcbce6c
Jordan Miller <Jordan.kay@gmail.com>
2021-12-23 09:28:29 -0700

    #43 I think this is a fix to the client null on start up problem, it waits to run the listener code until both connected and leaderchanges are populated, not sure if it's perfect because it's not a replay subject but I'll talk to Duane about it.

--- raven_back/lib/waiters/leader.dart
+++ raven_back/lib/waiters/leader.dart
@@ -32,99 +32,72 @@ class LeaderWaiter extends Waiter {
       autoDeinit: true,
     );
 
-    listen(
-      'streams.wallet.leaderChanges',
-      streams.wallet.leaderChanges,
-      (Change<Wallet> change) {
-        change.when(
-            loaded: (loaded) {},
-            added: (added) {
-              var leader = added.data;
-              if (ciphers.primaryIndex.getOne(leader.cipherUpdate) != null) {
-                services.wallet.leader
-                    .deriveMoreAddresses(leader as LeaderWallet);
-                services.client.subscribe.toExistingAddresses();
-              } else {
-                services.wallet.leader.backlog.add(leader as LeaderWallet);
-              }
-            },
-            updated: (updated) async {
-              /// when wallet is moved from one account to another...
-              /// we need to derive all the addresses again because it might
-              /// have moved from a testnet account to a mainnet account.
-              // remove addresses of the wallet
-              var leader = updated.data;
-              await addresses.removeAll(leader.addresses);
-              await balances
-                  .removeAll(balances.byWallet.getAll(leader.walletId));
+    // this is getting triggered before we have a client on occasion
+    // https://github.com/moontreeapp/moontree/issues/43
+    // .toExistingAddresses()
 
-              // remove the index from the registry
-              for (var exposure in [
-                NodeExposure.External,
-                NodeExposure.Internal
-              ]) {
-                services.wallet.leader.addressRegistry.remove(
+    //listen(
+    //  'streams.wallet.leaderChanges',
+    //  streams.wallet.leaderChanges,
+    //  (Change<Wallet> change) {
+
+    listen(
+      'streams.client.connected||streams.wallet.leaderChanges',
+      CombineLatestStream.combine2(
+        streams.client.connected,
+        streams.wallet.leaderChanges,
+        (bool connected, Change<Wallet> change) => Tuple2(connected, change),
+      ),
+      (Tuple2 tuple) {
+        bool connected = tuple.item1;
+        Change<Wallet> change = tuple.item2;
+        if (connected) {
+          change.when(
+              loaded: (loaded) {},
+              added: (added) {
+                var leader = added.data;
+                if (ciphers.primaryIndex.getOne(leader.cipherUpdate) != null) {
                   services.wallet.leader
-                      .addressRegistryKey(leader as LeaderWallet, exposure),
-                );
-              }
-              // recreate the addresses of that wallet
-              if (ciphers.primaryIndex.getOne(leader.cipherUpdate) != null) {
-                services.wallet.leader
-                    .deriveMoreAddresses(leader as LeaderWallet);
-                services.client.subscribe.toExistingAddresses();
-              } else {
-                services.wallet.leader.backlog.add(leader as LeaderWallet);
-              }
-            },
-            removed: (removed) {});
+                      .deriveMoreAddresses(leader as LeaderWallet);
+                  services.client.subscribe.toExistingAddresses();
+                } else {
+                  services.wallet.leader.backlog.add(leader as LeaderWallet);
+                }
+              },
+              updated: (updated) async {
+                /// when wallet is moved from one account to another...
+                /// we need to derive all the addresses again because it might
+                /// have moved from a testnet account to a mainnet account.
+                // remove addresses of the wallet
+                var leader = updated.data;
+                await addresses.removeAll(leader.addresses);
+                await balances
+                    .removeAll(balances.byWallet.getAll(leader.walletId));
+
+                // remove the index from the registry
+                for (var exposure in [
+                  NodeExposure.External,
+                  NodeExposure.Internal
+                ]) {
+                  services.wallet.leader.addressRegistry.remove(
+                    services.wallet.leader
+                        .addressRegistryKey(leader as LeaderWallet, exposure),
+                  );
+                }
+                // recreate the addresses of that wallet
+                if (ciphers.primaryIndex.getOne(leader.cipherUpdate) != null) {
+                  services.wallet.leader
+                      .deriveMoreAddresses(leader as LeaderWallet);
+                  services.client.subscribe.toExistingAddresses();
+                } else {
+                  services.wallet.leader.backlog.add(leader as LeaderWallet);
+                }
+              },
+              removed: (removed) {});
+        }
       },
       autoDeinit: true,
     );
-//
-    //listen(
-    //  'vouts/addresses',
-    //  CombineLatestStream.combine2(
-    //      vouts.changes
-    //          .where((change) => change is Added)
-    //          .map((change) => change.data),
-    //      addresses.changes
-    //          .where((change) => change is Added)
-    //          .map((change) => change.data),
-    //      (Vout vout, Address address) => Tuple2(vout, address)),
-    //  (Tuple2<Vout, Address> tuple) {
-    //    // if vout has wallet, derive,
-    //    //  (this means the address exists already so when the address was made
-    //    //  the vouts did not exist, and did not trigger an address derivation.)
-    //    // else if address has vout derive.
-    //    //  (this means it's vouts have been pull before the address was
-    //    //  created, which means when the vouts were created they had no
-    //    //  matching address, and didn't trigger an address derivation)
-    //    // if vout has no wallet and address has no vout don't derive.
-    //    var vout = tuple.item1;
-    //    var address = tuple.item2;
-    //    var wallet;
-    //    if (vout.wallet is LeaderWallet) {
-    //      wallet = vout.wallet as LeaderWallet;
-    //    } else if (address.wallet is LeaderWallet) {
-    //      wallet = address.wallet as LeaderWallet;
-    //    }
-    //    if (ciphers.primaryIndex.getOne(wallet.cipherUpdate) != null) {
-    //      var derived = services.wallet.leader.deriveMoreAddresses(
-    //        wallet,
-    //        exposures: [vout.address!.exposure],
-    //      );
-    //      services.client.subscribe.toExistingAddresses();
-    //      for (var addressId in derived.map((address) => address.addressId)) {
-    //        if (!services.address.retrieved.contains(addressId)) {
-    //          services.address.unretrieved.add(addressId);
-    //        }
-    //      }
-    //    } else {
-    //      services.wallet.leader.backlog.add(wallet);
-    //    }
-    //  },
-    //);
   }
 
   void attemptLeaderWalletAddressDerive(
