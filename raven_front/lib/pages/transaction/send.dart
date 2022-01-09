244165c03325b14dcffe64deb94b66156f107116
Brock-Wood <brocks.work@gmail.com>
2022-01-09 16:45:34 -0700

    removed unused dependency

--- raven_front/lib/pages/transaction/send.dart
+++ raven_front/lib/pages/transaction/send.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:tuple/tuple.dart';
 import 'package:url_launcher/url_launcher.dart';
