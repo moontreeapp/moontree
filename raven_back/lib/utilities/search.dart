cdd58a1f56b10e313eb8088eff73d79e57c08177
kralverde <2b2tminecrafter2@gmail.com>
2022-04-01 19:11:12 -0400

    #394 add binary insert

--- raven_back/lib/utilities/search.dart
+++ raven_back/lib/utilities/search.dart
@@ -1,3 +1,127 @@
+enum Nearest { FLOOR, CEIL, CLOSEST }
+
+int binaryClosest({
+  required List list,
+  required dynamic value,
+  Nearest type = Nearest.CLOSEST,
+  Comparator? comp,
+}) {
+  comp = comp ?? (a, b) => a - b;
+
+  // Quick returns
+  if (list.isEmpty) {
+    return -1;
+  }
+  // Our nearest value is less than the lowest value in the list
+  if (comp(value, list[0]) <= 0) {
+    if (type == Nearest.FLOOR) {
+      return -1;
+    }
+    return 0;
+  }
+  // Our nearest value is greater than the highest value in the list
+  if (comp(value, list[list.length - 1]) >= 0) {
+    if (type == Nearest.CEIL) {
+      return -1;
+    }
+    return list.length - 1;
+  }
+
+  // Modified binary search
+  int binarySearchInternal(List list, Comparator comp, dynamic value,
+      Nearest type, int start, int end) {
+    var i = 0;
+    var j = end;
+    var mid = 0;
+    while (i < j) {
+      mid = ((i + j) / 2).floor();
+      if (comp(value, list[mid]) == 0) {
+        return mid;
+      } else if (comp(value, list[mid]) < 0) {
+        if (mid > 0 && comp(value, list[mid - 1]) > 0) {
+          switch (type) {
+            case Nearest.FLOOR:
+              return mid - 1;
+            case Nearest.CEIL:
+              return mid;
+            case Nearest.CLOSEST:
+            default:
+              return comp(value, list[mid - 1]).abs() <
+                      comp(value, list[mid]).abs()
+                  ? mid - 1
+                  : mid;
+          }
+        }
+        j = mid;
+      } else {
+        if (mid < list.length - 1 && comp(value, list[mid + 1]) < 0) {
+          switch (type) {
+            case Nearest.FLOOR:
+              return mid;
+            case Nearest.CEIL:
+              return mid + 1;
+            case Nearest.CLOSEST:
+            default:
+              return comp(value, list[mid + 1]).abs() <
+                      comp(value, list[mid]).abs()
+                  ? mid + 1
+                  : mid;
+          }
+        }
+        i = mid + 1;
+      }
+    }
+
+    var mid_comp = comp(value, mid);
+    if (mid_comp < 0) {
+      if (mid > 0) {
+        switch (type) {
+          case Nearest.FLOOR:
+            return mid - 1;
+          case Nearest.CEIL:
+            return mid;
+          case Nearest.CLOSEST:
+          default:
+            return comp(value, list[mid - 1]).abs() <
+                    comp(value, list[mid]).abs()
+                ? mid - 1
+                : mid;
+        }
+      } else {
+        if (type == Nearest.FLOOR) {
+          return -1;
+        }
+        return mid;
+      }
+    }
+    if (mid_comp > 0) {
+      if (mid < list.length - 1) {
+        switch (type) {
+          case Nearest.FLOOR:
+            return mid;
+          case Nearest.CEIL:
+            return mid + 1;
+          case Nearest.CLOSEST:
+          default:
+            return comp(value, list[mid + 1]).abs() <
+                    comp(value, list[mid]).abs()
+                ? mid + 1
+                : mid;
+        }
+      } else {
+        if (type == Nearest.CEIL) {
+          return -1;
+        }
+        return mid;
+      }
+    }
+
+    return mid;
+  }
+
+  return binarySearchInternal(list, comp, value, type, 0, list.length - 1);
+}
+
 int binarySearch({
   required List list,
   required dynamic value,
@@ -50,19 +174,16 @@ bool binaryRemove({
   return false;
 }
 
-//bool binaryInsert({
-//  required List list,
-//  required dynamic value,
-//  Comparator? comp,
-//}) {
-//  var found = binarySearch(
-//    list: list,
-//    value: value,
-//    comp: comp,
-//  );
-//  if (found != -1) {
-//    list.insert(at, found);
-//    return true;
-//  }
-//  return false;
-//}
+void binaryInsert({
+  required List list,
+  required dynamic value,
+  Comparator? comp,
+}) {
+  var index =
+      binaryClosest(list: list, value: value, type: Nearest.CEIL, comp: comp);
+  if (index >= 0) {
+    list.insert(index, value);
+  } else {
+    list.add(value);
+  }
+}
--- raven_back/test/unit/utils/utilities.dart
+++ raven_back/test/unit/utils/utilities.dart
@@ -1,9 +1,12 @@
+import 'dart:math';
+
+import 'package:raven_back/utilities/search.dart';
 import 'package:test/test.dart';
 import '../../../lib/utilities/utilities.dart';
 
 void main() {
-  group('Binary removal', () {
-    test('test', () {
+  group('Binary', () {
+    test('test remove', () {
       final list = [];
       for (var i = 0; i < 1000; i++) {
         list.add(i);
@@ -22,6 +25,23 @@ void main() {
       }
       expect(utils.binaryRemove(list: list, comp: (a, b) => a - b, value: 1001),
           false);
+    });
+
+    test('binary insert', () {
+      for (var _ = 0; _ < 100; _++) {
+        final list = [];
+        final rand = Random();
+        final comp = (a, b) => (a - b) as int;
+        for (var i = 0; i < 1000; i++) {
+          list.add(rand.nextInt(1 << 31));
+        }
+        final list1 = [];
+        list.forEach((element) {
+          binaryInsert(list: list1, value: element, comp: comp);
+        });
+        list.sort(comp);
+        expect(list, list1);
+      }
     });
   });
 }
