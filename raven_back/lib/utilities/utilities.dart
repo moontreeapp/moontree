e0bdc19c090fdcb7f26d46e13379a7b5e8723b0f
kralverde <2b2tminecrafter2@gmail.com>
2022-04-01 17:24:26 -0400

    add a binary remove

--- raven_back/lib/utilities/utilities.dart
+++ raven_back/lib/utilities/utilities.dart
@@ -65,3 +65,25 @@ abstract class ToStringMixin {
   List<Object?> get props => [];
   List<String> get propNames => [];
 }
+
+void binaryRemove(List list, Comparator comp, dynamic value) {
+  if (list.isEmpty) {
+    return;
+  }
+
+  void binaryRemoveInternal(
+      List list, Comparator comp, dynamic value, int start, int end) {
+    if (start >= end) {
+      final mid = ((start + end) / 2).floor();
+      if (comp(value, list[mid]) == 0) {
+        list.removeAt(mid);
+      } else if (comp(value, list[mid]) > 0) {
+        binaryRemoveInternal(list, comp, value, mid + 1, end);
+      } else {
+        binaryRemoveInternal(list, comp, value, start, mid - 1);
+      }
+    }
+  }
+
+  binaryRemoveInternal(list, comp, value, 0, list.length - 1);
+}
