7460d6cb96ea718f496b1ea5c26833d01cbcb000
kralverde <2b2tminecrafter2@gmail.com>
2022-04-16 15:41:39 -0400

    #437 reset scripthashes on update

--- raven_back/lib/services/download/unspents.dart
+++ raven_back/lib/services/download/unspents.dart
@@ -5,13 +5,18 @@ import 'package:raven_back/raven_back.dart';
 
 /// we use the electrum server directly for determining our UTXO set
 class UnspentService {
-  Map<String, Set<ScripthashUnspent>> unspentsBySymbol = {};
+  // "Security name" -> address -> {unspents}
+  Map<String, Map<String, Set<ScripthashUnspent>>> unspentsBySymbol = {};
 
   String defaultSymbol(String? symbol) => symbol ?? 'RVN';
 
   Iterable<String> defaultScripthashes(Iterable<String>? scripthashes) =>
       scripthashes ??
       res.wallets.currentWallet.addresses.map((e) => e.scripthash).toList();
+
+  void clearUnspentsForScripthash(String scripthash, String symbol) {
+    unspentsBySymbol[symbol]?[scripthash]?.clear();
+  }
 
   void addUnspent({
     required String symbol,
@@ -23,7 +28,7 @@ class UnspentService {
         streams.app.triggers.add(ThresholdTrigger.backup);
       }
       if (!unspentsBySymbol.keys.contains(symbol)) {
-        unspentsBySymbol[symbol] = <ScripthashUnspent>{};
+        unspentsBySymbol[symbol] = <String, Set<ScripthashUnspent>>{};
         if (subscribe) {
           // Subscribe to a dummy asset of this type
           // This method checks if we're already subscribed and
@@ -38,34 +43,68 @@ class UnspentService {
               position: 0));
         }
       }
-      unspentsBySymbol[symbol]!.addAll(unspents);
+      for (var unspent in unspents) {
+        if (!unspentsBySymbol[symbol]!.containsKey(unspent.scripthash)) {
+          unspentsBySymbol[symbol]![unspent.scripthash] = <ScripthashUnspent>{};
+        }
+        unspentsBySymbol[symbol]![unspent.scripthash]!.add(unspent);
+      }
     }
   }
 
   Future<void> pull({Iterable<String>? scripthashes, bool? updateRVN}) async {
     scripthashes = defaultScripthashes(scripthashes);
+
     var rvn = 'RVN';
     if (updateRVN ?? true) {
       var utxos = (await services.client.client!.getUnspents(scripthashes))
           .expand((i) => i);
+      // Wipe relevant unspents and re-add
+      scripthashes.forEach((x) => clearUnspentsForScripthash(x, rvn));
       addUnspent(symbol: rvn, unspents: utxos);
     }
     if (!(updateRVN ?? false)) {
       var utxos = (await services.client.client!.getAssetUnspents(scripthashes))
           .expand((i) => i);
-      for (var utxo in utxos) {
+
+      // Parse it into something more digestable
+      final downloaded = <String, Map<String, Set<ScripthashUnspent>>>{};
+      for (final utxo in utxos) {
         if (utxo.symbol != null) {
-          addUnspent(symbol: utxo.symbol!, unspents: {utxo}, subscribe: true);
+          // Should never be null but can't hurt to be safe
+          if (!downloaded.containsKey(utxo.symbol!)) {
+            downloaded[utxo.symbol!] = <String, Set<ScripthashUnspent>>{};
+          }
+          if (!downloaded[utxo.symbol!]!.containsKey(utxo.scripthash)) {
+            downloaded[utxo.symbol!]![utxo.scripthash] = <ScripthashUnspent>{};
+          }
+          downloaded[utxo.symbol!]![utxo.scripthash]!.add(utxo);
+        }
+      }
+
+      // And then remove and add
+      for (final symbol in downloaded.keys) {
+        final scripthashes_internal = downloaded[symbol]!.keys;
+        scripthashes_internal
+            .forEach((x) => clearUnspentsForScripthash(x, symbol));
+        for (final scripthash_internal in scripthashes_internal) {
+          addUnspent(
+              symbol: symbol,
+              unspents: downloaded[symbol]![scripthash_internal]!,
+              subscribe: true);
         }
       }
     }
   }
 
-  int total([String? symbol]) =>
-      unspentsBySymbol.keys.contains(defaultSymbol(symbol))
-          ? unspentsBySymbol[defaultSymbol(symbol)]!.fold(
-              0, (int total, ScripthashUnspent item) => item.value + total)
-          : 0;
+  // TODO: Maybe cache instead of calculating each time?
+  int total([String? symbol]) => unspentsBySymbol.keys
+          .contains(defaultSymbol(symbol))
+      ? unspentsBySymbol[defaultSymbol(symbol)]!
+          .values // List of iterable of items
+          .expand((element) => element) // Flatten the list of iterables
+          .fold(0, (int total, ScripthashUnspent item) => item.value + total)
+      : 0;
 
   void assertSufficientFunds(int amount, String? symbol) {
     if (total(defaultSymbol(symbol)) < amount) {
@@ -74,5 +113,9 @@ class UnspentService {
   }
 
   Set<ScripthashUnspent> getUnspents(String? symbol) =>
-      unspentsBySymbol[defaultSymbol(symbol)] ?? <ScripthashUnspent>{};
+      unspentsBySymbol[defaultSymbol(symbol)]
+          ?.values
+          .expand((element) => element)
+          .toSet() ??
+      <ScripthashUnspent>{};
 }
