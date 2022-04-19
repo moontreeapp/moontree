12c9396ad67c85b3346b0ee7d7238e67f17d84ac
Jordan Miller <Jordan.kay@gmail.com>
2022-04-19 17:52:54 -0600

    x close

--- raven_front/lib/widgets/front/coming_soon.dart
+++ raven_front/lib/widgets/front/coming_soon.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:raven_front/components/components.dart';
 import 'package:raven_front/theme/colors.dart';
 import 'package:raven_front/utils/zips.dart';
-import 'package:raven_front/widgets/widgets.dart';
 
 class ComingSoonPlaceholder extends StatelessWidget {
   final String message;
