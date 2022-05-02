298d10912676af4cf03f48d2adafd0fc49ca6dcb
Jordan Miller <Jordan.kay@gmail.com>
2022-05-02 07:36:07 -0600

    #520 works

--- raven_front/lib/widgets/top/activity.dart
+++ raven_front/lib/widgets/top/activity.dart
@@ -5,7 +5,6 @@ import 'package:lottie/lottie.dart';
 import 'package:raven_back/raven_back.dart';
 import 'package:raven_back/streams/client.dart';
 import 'package:raven_front/components/components.dart';
-import 'package:raven_front/theme/theme.dart';
 
 class ActivityLight extends StatefulWidget {
   ActivityLight({Key? key}) : super(key: key);
@@ -17,14 +16,17 @@ class ActivityLight extends StatefulWidget {
 class _ActivityLightState extends State<ActivityLight>
     with TickerProviderStateMixin {
   List<StreamSubscription> listeners = [];
-  ConnectionStatus connectionStatus = ConnectionStatus.disconnected;
-  Color connectionStatusColor = AppColors.error;
   bool connectionBusy = false;
+  ActivityMessage activityMessage = ActivityMessage();
 
   @override
   void initState() {
     super.initState();
-
+    listeners.add(streams.client.activity.listen((ActivityMessage value) async {
+      if (activityMessage != value) {
+        setState(() => activityMessage = value);
+      }
+    }));
     listeners.add(streams.client.busy.listen((bool value) async {
       if (!connectionBusy && value) {
         setState(() => connectionBusy = value);
@@ -45,16 +47,15 @@ class _ActivityLightState extends State<ActivityLight>
 
   @override
   Widget build(BuildContext context) {
-    ActivityMessage activityMessage = streams.client.activity.value;
     return connectionBusy
         ? GestureDetector(
             onTap: () => components.message.giveChoices(
-              context,
+              components.navigator.routeContext!,
               title: activityMessage.title,
               content: activityMessage.message,
               behaviors: {
                 'ok': () {
-                  Navigator.of(context).pop();
+                  Navigator.of(components.navigator.routeContext!).pop();
                 },
               },
             ),
