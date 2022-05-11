d3a47e903b2f655de5920ae55b48969288517943
Jordan Miller <Jordan.kay@gmail.com>
2022-05-11 08:03:58 -0600

    #568 made helper functions for later use

--- raven_front/lib/utils/extensions.dart
+++ raven_front/lib/utils/extensions.dart
@@ -1,11 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:raven_front/utils/size.dart';
+import 'package:raven_front/components/components.dart';
+
+/*
+.ofMediaHeight(context)  // for use in builders
+.ofMediaWidth(context)  // for use in builders
+.ofScreenHeight        // entire screen  
+.ofScreenWidth        // entire screen
+.ofSafeHeight        // entire screen minus system toolbar
+.ofAppHeight        // entire screen minus system toolbar and fixed app bar
+*/
 
 extension RelativeHeightDoble on double {
   double figma(BuildContext context) => figmaHeight(context, this);
   double ofMediaHeight(BuildContext context) => relativeHeight(context, this);
   double ofMediaHeightMinusNavbar(BuildContext context, {bool tall = true}) =>
       relativeHeight(context, this, heightMinus(context, tall ? 118 : 72));
+  double get ofScreenHeight => relativeHeight(
+        components.navigator.routeContext!,
+        this,
+      );
+  double get ofSafeHeight =>
+      relativeHeight(
+        components.navigator.routeContext!,
+        this,
+      ) -
+      24;
+  double get ofAppHeight =>
+      relativeHeight(
+        components.navigator.routeContext!,
+        this,
+      ) -
+      (24 + 56);
+  double ofMediaWidth(BuildContext context) => relativeWidth(context, this);
+  double get ofScreenWidth => relativeWidth(
+        components.navigator.routeContext!,
+        this,
+      );
 }
 
 extension RelativeHeightInt on int {
@@ -13,4 +44,25 @@ extension RelativeHeightInt on int {
   double ofMediaHeight(BuildContext context) => relativeHeight(context, this);
   double ofMediaHeightMinusNavbar(BuildContext context, {bool tall = true}) =>
       relativeHeight(context, this, heightMinus(context, tall ? 118 : 72));
+  double get ofScreenHeight => relativeHeight(
+        components.navigator.routeContext!,
+        this,
+      );
+  double get ofSafeHeight =>
+      relativeHeight(
+        components.navigator.routeContext!,
+        this,
+      ) -
+      24;
+  double get ofAppHeight =>
+      relativeHeight(
+        components.navigator.routeContext!,
+        this,
+      ) -
+      (24 + 56);
+  double ofMediaWidth(BuildContext context) => relativeWidth(context, this);
+  double get ofScreenWidth => relativeWidth(
+        components.navigator.routeContext!,
+        this,
+      );
 }
--- raven_front/lib/utils/size.dart
+++ raven_front/lib/utils/size.dart
@@ -7,9 +7,17 @@ double figmaHeight(context, height) =>
 /// converts screen percentage to a pixel height
 /// or pixel height to screen percentage
 /// to be used with (draggable widgets since they take % of screen)
-double relativeHeight(context, height, [double? givenHeight]) => height > 1.0
-    ? height / (givenHeight ?? MediaQuery.of(context).size.height)
-    : height * (givenHeight ?? MediaQuery.of(context).size.height);
+double relativeHeight(context, num height, [double? givenHeight]) =>
+    height > 1.0
+        ? height / (givenHeight ?? MediaQuery.of(context).size.height)
+        : height * (givenHeight ?? MediaQuery.of(context).size.height);
+
+/// converts screen percentage to a pixel height
+/// or pixel height to screen percentage
+/// to be used with (draggable widgets since they take % of screen)
+double relativeWidth(context, num width, [double? givenWidth]) => width > 1.0
+    ? width / (givenWidth ?? MediaQuery.of(context).size.width)
+    : width * (givenWidth ?? MediaQuery.of(context).size.width);
 
 double heightMinus(context, double height) =>
     MediaQuery.of(context).size.height - relativeHeight(context, height);
