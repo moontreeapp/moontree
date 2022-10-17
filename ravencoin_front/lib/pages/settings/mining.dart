ab66d5d6ccca7788017cc56b5826bdc4f8531483
Jordan Miller <Jordan.kay@gmail.com>
2022-10-17 10:13:56 -0600

    conformed miner mode

--- ravencoin_front/lib/pages/settings/mining.dart
+++ ravencoin_front/lib/pages/settings/mining.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ravencoin_front/widgets/widgets.dart';
 
+import 'package:ravencoin_back/ravencoin_back.dart';
+
 class MiningChoice extends StatelessWidget {
   const MiningChoice() : super();
 
@@ -21,6 +23,14 @@ class MiningChoice extends StatelessWidget {
                 padding:
                     EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 16),
                 child: Container(
-                    alignment: Alignment.topLeft, child: MinerModeChoice()))),
+                    alignment: Alignment.topLeft,
+                    child: SwtichChoice(
+                      label: 'Miner Mode',
+                      description:
+                          'In Miner Mode, full transaction histories are not downloaded. This makes syncing your wallets faster.',
+                      initial: services.wallet.currentWallet.minerMode,
+                      onChanged: (value) =>
+                          services.download.queue.setMinerMode(value),
+                    )))),
       ]);
 }
