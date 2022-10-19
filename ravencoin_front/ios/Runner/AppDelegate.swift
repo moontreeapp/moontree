6aa6c0975001757d0c50a07da87cfbba2f8bb5b0
Jordan Miller <Jordan.kay@gmail.com>
2022-10-19 12:26:17 -0600

    Rc (#656)
    * pbxproj from latest build
    
    * iOS build changes
    
    Co-authored-by: Brock-Wood <brocks.work@gmail.com>

--- ravencoin_front/ios/Runner/AppDelegate.swift
+++ ravencoin_front/ios/Runner/AppDelegate.swift
@@ -1,38 +1,3 @@
-/* disable screen capture app wide for ios:
-/// REPLACEMENT 
-/// BY https://stackoverflow.com/questions/52317217/flutter-disable-screenshot-capture-for-app
-/// FOR https://github.com/moontreeapp/moontree/issues/521
-*/
-/*
-import UIKit
-import Flutter
-
-@UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-    override func application(
-      _ application: UIApplication,
-      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-      self.window.makeSecure()
-      GeneratedPluginRegistrant.register(with: self)
-      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-}
-
-extension UIWindow {
-func makeSecure() {
-    let field = UITextField()
-    field.isSecureTextEntry = true
-    self.addSubview(field)
-    field.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-    field.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-    self.layer.superlayer?.addSublayer(field.layer)
-    field.layer.sublayers?.first?.addSublayer(self.layer)
-  }
-}
-
-Original 
-*/
 import UIKit
 import Flutter
 
