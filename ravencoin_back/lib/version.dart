4f8ce16c7472651d1fd29db4a2c8a6b3f542f586
Jordan Miller <Jordan.kay@gmail.com>
2022-11-13 14:05:55 -0700

    updated version to 1.6.5

--- ravencoin_back/lib/version.dart
+++ ravencoin_back/lib/version.dart
@@ -2,8 +2,8 @@
 /// version.py script looks at this map.
 /// so set the versions but don't alter the code structure.
 const VERSIONS = {
-  'ios': {'alpha': '1.3.10+11~1', 'beta': '1.6.0+21~1'},
-  'android': {'alpha': '1.0.1+2~1', 'beta': '1.6.0+5~1'}
+  'ios': {'alpha': '1.3.10+11~1', 'beta': '1.6.5+21~1'},
+  'android': {'alpha': '1.0.1+2~1', 'beta': '1.6.5+5~1'}
 };
 
 const VERSION_TRACK = 'beta';
