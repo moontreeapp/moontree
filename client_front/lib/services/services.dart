9b71d369369aad4b45dcfa5d7e8d86c882dd2edc
Jordan Miller <Jordan.kay@gmail.com>
2023-01-20 11:34:29 -0700

    cleanup

--- client_front/lib/services/services.dart
+++ client_front/lib/services/services.dart
@@ -1,9 +1,3 @@
 import 'package:client_front/services/hive.dart';
-import 'nav.dart';
 
 final DataLoadingHelper HIVE_INIT = DataLoadingHelper();
-
-// ignore: avoid_classes_with_only_static_members
-class favors {
-  static final NavWrap nav = NavWrap();
-}
