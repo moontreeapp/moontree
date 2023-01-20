4f7ec520c1594a2a716cefe5e63ec36bd4effd65
Jordan Miller <Jordan.kay@gmail.com>
2023-01-20 11:14:51 -0700

    testing

--- client_front/lib/services/nav.dart
+++ client_front/lib/services/nav.dart
@@ -1,3 +1,5 @@
+/// experiment with manually handling transition logic and animation
+
 import 'package:flutter/material.dart';
 import 'package:client_front/components/components.dart';
 import 'package:client_front/pages/pages.dart';
@@ -32,8 +34,18 @@ class NavWrap {
             pageBuilder: (context, animation1, animation2) =>
                 pages.routes(components.routes.routeContext!)[link]!(
                     components.routes.routeContext!), //Page1(),
-            transitionDuration: Duration(seconds: 1),
-            reverseTransitionDuration: Duration(seconds: 1),
+            transitionDuration: fadeDuration,
+            reverseTransitionDuration: fadeDuration,
+            transitionsBuilder: (_, animation, secondaryAnimation, child) =>
+                FadeTransition(
+              opacity: animation.drive(Tween<double>(begin: 0, end: 1)
+                  .chain(CurveTween(curve: const DelayedCurve()))),
+              child: FadeTransition(
+                  opacity: secondaryAnimation.drive(
+                      Tween<double>(begin: 1, end: 0)
+                          .chain(CurveTween(curve: const CurveDelayed()))),
+                  child: child),
+            ),
           ),
         ),
         before: () => print('before'),
@@ -45,3 +57,32 @@ class NavWrap {
 
 /*
 */
+
+const slideDuration = Duration(milliseconds: 3000);
+const fadeDuration = Duration(milliseconds: 3000);
+
+class DelayedCurve extends Curve {
+  const DelayedCurve() : super();
+
+  @override
+  double transformInternal(double t) {
+    if (t < 0.5) {
+      return 0.0;
+    } else {
+      return (t - 0.5) * 2.0;
+    }
+  }
+}
+
+class CurveDelayed extends Curve {
+  const CurveDelayed() : super();
+
+  @override
+  double transformInternal(double t) {
+    if (t > 0.5) {
+      return 1.0;
+    } else {
+      return t * 2;
+    }
+  }
+}
