49ed0a06c9757218a56d93faf3dd544d163daf3c
Jordan Miller <Jordan.kay@gmail.com>
2023-03-21 09:08:40 -0600

    cleanup

--- client_back/lib/services/transaction/transaction.dart
+++ client_back/lib/services/transaction/transaction.dart
@@ -52,12 +52,16 @@ extension TransactionViewMethods on TransactionView {
   //    type == TransactionViewType.self ? iReceived : iValue;
   /// just the total that went away or came in
   int get totalValue => outgoing ? iProvided : iReceived;
-  int get iValueTotal => !isCoin &&
+  int get iValueTotal {
+    print(
+        'height: ${height}, iProvided: ${iProvided}, iReceived: ${iReceived}');
+    return !isCoin &&
             [TransactionViewType.self, TransactionViewType.consolidation]
                 .contains(type) &&
             height > 0
         ? iReceived
         : iValue;
+  }
 
   /// iValue and sent to self on assets always shows 0 since tx fees are in the base currency...
   /// Using iReceived is not technically any better because it just reflects the
