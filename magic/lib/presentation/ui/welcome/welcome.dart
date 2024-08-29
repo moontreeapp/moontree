037176771e9bc79fa5bd59b8aba69d3e46c10580
Jordan Miller <Jordan.kay@gmail.com>
2024-08-29 12:46:15 -0600

    fix?

--- magic/lib/presentation/ui/welcome/welcome.dart
+++ magic/lib/presentation/ui/welcome/welcome.dart
@@ -10,17 +10,11 @@ import 'package:magic/cubits/welcome/cubit.dart';
 import 'package:magic/domain/blockchain/blockchain.dart';
 import 'package:magic/presentation/theme/colors.dart';
 import 'package:magic/presentation/utils/animation.dart';
+import 'package:magic/presentation/widgets/animations/fading.dart';
 import 'package:magic/presentation/widgets/assets/icons.dart';
 import 'package:magic/services/services.dart';
 import 'package:magic/services/security.dart';
 import 'package:url_launcher/url_launcher_string.dart'; // Make sure to import the security service
-
-// Add this function at the top level of your file
-Future<void> precacheSvgPicture(String assetName) async {
-  final loader = SvgAssetLoader(assetName);
-  await svg.cache
-      .putIfAbsent(loader.cacheKey(null), () => loader.loadBytes(null));
-}
 
 class WelcomeLayer extends StatelessWidget {
   const WelcomeLayer({super.key});
@@ -39,14 +33,14 @@ class WelcomeLayer extends StatelessWidget {
 }
 
 class WelcomeBackScreen extends StatefulWidget {
-  const WelcomeBackScreen({super.key});
+  final SvgPicture? imageWidget;
+  const WelcomeBackScreen({super.key, this.imageWidget});
 
   @override
   WelcomeBackScreenState createState() => WelcomeBackScreenState();
 }
 
 class WelcomeBackScreenState extends State<WelcomeBackScreen> {
-  late Future<void> _imageFuture;
   double _fadingInValue = 1;
   bool _isAnimating = false;
   bool _isFading = false;
@@ -178,48 +172,42 @@ class WelcomeBackScreenState extends State<WelcomeBackScreen> {
   @override
   void initState() {
     super.initState();
-    _imageFuture = precacheSvgPicture(LogoIcons.magic);
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _imageFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState != ConnectionState.done) {
-          return const Center(child: CircularProgressIndicator());
-        }
-        return AnimatedOpacity(
-          opacity: _isFadingOut ? 0 : 1,
-          duration: slowFadeDuration,
-          child: Stack(
-            children: <Widget>[
-              AnimatedPositioned(
+    return AnimatedOpacity(
+      opacity: _isFadingOut ? 0 : 1,
+      duration: slowFadeDuration,
+      child: Stack(
+        children: <Widget>[
+          AnimatedPositioned(
+            duration: slowFadeDuration,
+            curve: Curves.easeInOutCubic,
+            top: _isAnimating ? screen.height - screen.pane.midHeight : 0,
+            left: 0,
+            right: 0,
+            child: AnimatedContainer(
+              duration: slowFadeDuration,
+              curve: Curves.easeOutCubic,
+              alignment: Alignment.center,
+              height: screen.height,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(_isAnimating ? 30 : 0)),
+                  color: AppColors.foreground),
+              child: AnimatedOpacity(
+                opacity: _isFading ? 0 : _fadingInValue,
                 duration: slowFadeDuration,
-                curve: Curves.easeInOutCubic,
-                top: _isAnimating ? screen.height - screen.pane.midHeight : 0,
-                left: 0,
-                right: 0,
-                child: AnimatedContainer(
-                  duration: slowFadeDuration,
-                  curve: Curves.easeOutCubic,
-                  alignment: Alignment.center,
-                  height: screen.height,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.vertical(
-                          top: Radius.circular(_isAnimating ? 30 : 0)),
-                      color: AppColors.foreground),
-                  child: AnimatedOpacity(
-                    opacity: _isFading ? 0 : _fadingInValue,
-                    duration: slowFadeDuration,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Expanded(
-                          child: Center(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Expanded(
+                      child: Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            widget.imageWidget ??
                                 Padding(
                                     padding: const EdgeInsets.only(
                                         top: 8.0, bottom: 0),
@@ -229,54 +217,51 @@ class WelcomeBackScreenState extends State<WelcomeBackScreen> {
                                       fit: BoxFit.contain,
                                       alignment: Alignment.center,
                                     )),
-                                const SizedBox(height: 8),
-                                const Text(
-                                  'Welcome',
-                                  style: TextStyle(
-                                    fontSize: 24,
-                                    color: AppColors.subtitle,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: SizedBox(
-                            width: double.infinity,
-                            height: 60,
-                            child: ElevatedButton(
-                              onHover: (_) => cubits.app.animating = true,
-                              onPressed:
-                                  _handleAuthentication, // Changed this line
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: AppColors.button,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30),
-                                ),
-                              ),
-                              child: const Text(
-                                "LET'S GO",
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                            const SizedBox(height: 8),
+                            const Text(
+                              'Welcome',
+                              style: TextStyle(
+                                fontSize: 24,
+                                color: AppColors.subtitle,
                               ),
                             ),
-                          ),
+                          ],
                         ),
-                        const SizedBox(height: 16),
-                      ],
+                      ),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 60,
+                        child: ElevatedButton(
+                          onHover: (_) => cubits.app.animating = true,
+                          onPressed: _handleAuthentication, // Changed this line
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: AppColors.button,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                          ),
+                          child: const Text(
+                            "LET'S GO",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                  ],
                 ),
               ),
-            ],
+            ),
           ),
-        );
-      },
+        ],
+      ),
     );
   }
 }
